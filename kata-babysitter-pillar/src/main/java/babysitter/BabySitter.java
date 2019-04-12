8a77b7311830c4c59681f048f2e2fd6ce7594720
jeandps <49538962+jeandps@users.noreply.github.com>
2019-04-12 02:27:29 +0200

    First test of BabySitter with Start Time

--- kata-babysitter-pillar/src/main/java/babysitter/BabySitter.java
+++ kata-babysitter-pillar/src/main/java/babysitter/BabySitter.java
@@ -1,5 +1,19 @@
 package babysitter;
 
 public class BabySitter {
+	
+	private int iStartTime;
+	
+	public BabySitter() {
+		setiStartTime(17);
+	}
+
+	public int getiStartTime() {
+		return iStartTime;
+	}
+
+	public void setiStartTime(int iStartTime) {
+		this.iStartTime = iStartTime;
+	}
 
 }
--- kata-babysitter-pillar/src/test/java/babysitter/BabySitterTest.java
+++ kata-babysitter-pillar/src/test/java/babysitter/BabySitterTest.java
@@ -1,5 +1,15 @@
 package babysitter;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class BabySitterTest {
+	
+	@Test
+	public void babySitterReturnsStartTimeAs17() {
+		BabySitter myBabySitter = new BabySitter();
+		assertEquals(17, myBabySitter.getiStartTime()); 
+	}
 
 }
